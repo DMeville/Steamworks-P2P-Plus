8cb6253b09e0fb8fdfdc76a55cf8d3179e8f3f05
Dylan Meville <dylan@dylanmeville.com>
2018-03-19 18:54:32 -0600

    Core Manager
    little easier to use, less to type

---
+++ Assets/Networking/Core.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//I use singletons too much. This is for ease of access and I'm lazy. 
+//Easier to type Core.net.SendMessage() vs NetworkManager.instance.SendMessage()
+//just an easy way to store and access any manager objects
+public class Core : MonoBehaviour {
+
+    public static NetworkManager net;
+    //public static OtherManager other; etc etc
+}
